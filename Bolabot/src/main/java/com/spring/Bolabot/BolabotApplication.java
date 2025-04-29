838b54f94e39dd72a3041f9176b5156b685d62de
Aditi Mahabole <akmbole2002@gmail.com>
2025-04-29 21:38:25 +0530

    Adding Springboot Project using Spring Initializer

---
+++ Bolabot/src/main/java/com/spring/Bolabot/BolabotApplication.java
@@ -0,0 +1,13 @@
+package com.spring.Bolabot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BolabotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BolabotApplication.class, args);
+	}
+
+}
---
+++ Bolabot/src/test/java/com/spring/Bolabot/BolabotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.Bolabot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BolabotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
